00a07555fc461973f31ee1b00a766f1b48ca93ef
Hashim Hikmah Omonike <hashimhikmah09@gmail.com>
2025-11-25 09:39:25 +0100

    background image added to book appointment page

--- oh/app/book-services/page.tsx
+++ oh/app/book-services/page.tsx
@@ -14,6 +14,7 @@ export default function BookServicesPage() {
       <Hero
         title="Book Your Appointment"
         description="Fill out the form below to request a booking. We'll get back to you to confirm your appointment."
+        image="https://lh3.googleusercontent.com/aida-public/AB6AXuB_csFz9j5Dee86UdXPpn_9P6Al3rLszCLqAXcqCx0tX_RoumFdDHumzNBSb5UqWEoK-rO7UwTWf1t1-K5caquHHASVqhnpJhsFRuykQ4h4-k1NIYqWNpSpFHOHz2Fgp6WEmeh7X3TbJ6BkXBhOa0tQjyhj7joChIOF6Lqe1AiktM1r0fDVgHj2mbIdp8L9QEgFWKV0jWgxuUZYKigD8h7vOYmA2wDQg19JT6ffOpipRVU1TZWm9XBZy9WTc7NAQNum1eZiY0e5gu6b"
       />
 
       <section className="py-16 bg-white">
